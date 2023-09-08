24ba94d665b5b9d8f7bd7f2a15e036ead016bb67
mcsymiv <s.mcsymiv@gmail.com>
2023-09-08 12:43:00 +0300

    gofmt format

--- format/format.go
+++ format/format.go
@@ -10,56 +10,56 @@ import (
 // Usage: FormatFiles("./data/", "suite")
 func FormatFiles(root, filename string) {
 	files, _ := file.FindFiles(root, filename)
-  for _, f := range files {
-    ctxs := []ctx.Context{
+	for _, f := range files {
+		ctxs := []ctx.Context{
 
-      // removes lines contains key word
-      // clears passed tests
-      &actions.RemoveSubLine{
-        RemoveLine: &actions.RemoveLine{
-          Line: "passed",
-        },
-      },
+			// removes lines contains key word
+			// clears passed tests
+			&actions.RemoveSubLine{
+				RemoveLine: &actions.RemoveLine{
+					Line: "passed",
+				},
+			},
 
-      // clears skipped tests
-      &actions.RemoveSubLine{
-        RemoveLine: &actions.RemoveLine{
-          Line: "skipped",
-        },
-      },
+			// clears skipped tests
+			&actions.RemoveSubLine{
+				RemoveLine: &actions.RemoveLine{
+					Line: "skipped",
+				},
+			},
 
-      // replaces csv default deliminer with a pipe token for convenience
-      &actions.ReplaceWord{
-        ReplaceLine: &actions.ReplaceLine{
-          Old: "\",\"",
-          New: "|",
-        },
-      },
+			// replaces csv default deliminer with a pipe token for convenience
+			&actions.ReplaceWord{
+				ReplaceLine: &actions.ReplaceLine{
+					Old: "\",\"",
+					New: "|",
+				},
+			},
 
-      // removes columns with redundant data
-      &actions.RemoveColumn{
-        Deliminer: "|",
-        RemoveLine: &actions.RemoveLine{
-          Line: "1,2,3,4,6,7,8,10",
-        },
-      },
+			// removes columns with redundant data
+			&actions.RemoveColumn{
+				Deliminer: "|",
+				RemoveLine: &actions.RemoveLine{
+					Line: "1,2,3,4,6,7,8,10",
+				},
+			},
 
-      // replaces back pipe token to csv deliminer
-      &actions.ReplaceWord{
-        ReplaceLine: &actions.ReplaceLine{
-          Old: "|",
-          New: "\",\"",
-        },
-      },
-      &actions.AddEndLine{
-        Line: "\"",
-      },
-    }
+			// replaces back pipe token to csv deliminer
+			&actions.ReplaceWord{
+				ReplaceLine: &actions.ReplaceLine{
+					Old: "|",
+					New: "\",\"",
+				},
+			},
+			&actions.AddEndLine{
+				Line: "\"",
+			},
+		}
 
-    for _, ct := range ctxs {
-      ctx.Exec(ct, f.FilePath)
-    }
+		for _, ct := range ctxs {
+			ctx.Exec(ct, f.FilePath)
+		}
 
-    ctx.Sort(f.FilePath)
-  }
+		ctx.Sort(f.FilePath)
+	}
 }
